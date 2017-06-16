182b030ee8755756d40529f271b1fe77750af46f
harishkandan <harishkandan95@gmail.com>
2017-06-16 19:31:15 +0000

    Done

--- build.py
+++ build.py
@@ -2,13 +2,18 @@ import operator
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    ascending_ans=[]
+    asc=dic.keys()
+    asc.sort()
+    for i in range(0,len(asc)):
+        ascending_ans.append((asc[i],dic[asc[i]]))
+    return ascending_ans
 
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
-
+    descending_ans=[]
+    dsc=dic.keys()
+    dsc.sort()
+    for i in range(len(dsc),0,-1):
+        descending_ans.append((dsc[i-1],dic[dsc[i-1]]))
+    return descending_ans
