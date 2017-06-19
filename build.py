bf8ccefc4f0981c481f016d68c82cda993bc2696
harishkandan <harishkandan95@gmail.com>
2017-06-19 04:29:04 +0000

    Done

--- build.py
+++ build.py
@@ -1,19 +1,6 @@
-import operator
-
-
 def solution_asc(dic):
-    ascending_ans=[]
-    asc=dic.keys()
-    asc.sort()
-    for i in range(0,len(asc)):
-        ascending_ans.append((asc[i],dic[asc[i]]))
-    return ascending_ans
+    return sorted(dic.items())
 
 
 def solution_desc(dic):
-    descending_ans=[]
-    dsc=dic.keys()
-    dsc.sort()
-    for i in range(len(dsc),0,-1):
-        descending_ans.append((dsc[i-1],dic[dsc[i-1]]))
-    return descending_ans
+    return sorted(dic.items(),reverse=True)
